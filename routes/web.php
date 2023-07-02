71a1d0511c6300fdbdcfda211211491df015ef9d
anfahrul <an.fahrulan@gmail.com>
2023-07-02 14:43:41 +0800

    solved some error on welcome endpoint

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     // return view('welcome');
     return response()->json([
-        'status' => 'OK'
+        'status' => 'OK',
         'message' => 'Selamat datang di Umkmart APIs'
     ], 200);
 });
